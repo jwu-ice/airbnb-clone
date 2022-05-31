d2e1fe7c2cd417e8ef460f5f1cc3eb814305a6dd
jwu-ice <sju02048@naver.com>
2022-05-31 17:36:35 +0900

    :lipstick: 폰트추가 및 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import GlobalStyles from "@styles/GlobalStyles";
-import { theme } from "@styles/theme";
 import React from "react";
 
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
+
+import GlobalStyles from "@styles/GlobalStyles";
+import { theme } from "@styles/theme";
 
 import App from "./App";
 
